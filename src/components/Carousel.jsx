25ab496b211fe94f96d351aab767ea528f7a83b1
Zion203 <jesudas.t@kalvium.community>
2024-01-05 18:22:53 +0530

    commit

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,7 +7,56 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
+  constructor(props) {
+    super(props)
   
+    this.state = {
+       index :2
+    }
+  }
+
+  
+  render(){
+
+    let nexter = ()=>{
+        if (this.state.index!=2){
+            this.setState((prev)=>({
+                index : prev.index+1
+            })
+        )}else{
+            this.setState({
+                index :0
+            })
+        }
+      }
+
+    let perver = () =>{
+        if (this.state.index!=0){
+            this.setState((prev)=>({
+                index : prev.index-1
+            })
+        )}else{
+            this.setState({
+                index:2
+            })
+        }
+    }
+    return(
+        <div id="main">
+            <ArrowBackIosIcon onClick ={perver} />
+            <div>
+                <img src={images[this.state.index].img} alt="" />
+                <div id="text-img">
+                    <h1 id="title" className="texts">{images[this.state.index].title}</h1>
+                    <p className="texts" id="subtitle">{images[this.state.index].subtitle}</p>
+                </div>
+            </div>
+            <ArrowForwardIosIcon onClick={nexter} />
+
+
+        </div>
+    )
+  }
 }
 
 export default Carousel;
